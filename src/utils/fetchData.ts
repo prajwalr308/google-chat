94948d5e91c7c096ad1680b34ce065882b2c7c1f
prajwal <prajwalr.thedevelper@gmail.com>
2023-10-23 10:20:08 +0530

    fix: production logic

--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -1,7 +1,9 @@
-export const getData = async(): Promise<any | null> => {
-  const response = await fetch(
-    "https://google-chat-tan.vercel.app" || "http://localhost:3000" + "/api/getMessages"
-  );
+export const getData = async (): Promise<any | null> => {
+  const apiUrl =
+    process.env.NODE_ENV === "production"
+      ? "https://google-chat-tan.vercel.app/api/getMessages"
+      : "http://localhost:3000/api/getMessages";
+  const response = await fetch(apiUrl);
   if (!response.ok) throw new Error("Error fetching data");
   const data = await response.json();
   return data || null;
